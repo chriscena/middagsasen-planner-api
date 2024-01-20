95097f911bedcd59d9ba39fa9b76bcf096bbeb75
Christoffer Cena <chris+github@cenaonline.net>
2024-01-21 00:23:36 +0100

    #20 Lagt til ny tabell.

---
+++ src/Middagsasen.Planner.Api/Data/EventResourceMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Middagsasen.Planner.Api.Data
+{
+    public class EventResourceMessage
+    {
+        public int EventResourceMessageId { get; set; }
+        public int EventResourceId { get; set;}
+        public string Message { get; set; } = null!;
+        public int Created { get; set; }
+        public DateTime CreatedBy { get; set; }
+
+        public User CreatedByUser { get; set; } = null!;
+        public EventResource Resource { get; set; } = null!;
+    }
+}
